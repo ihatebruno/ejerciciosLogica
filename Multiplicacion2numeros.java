7459616db28f6ac78b321fac837ce93273f64623
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-04-18 18:42:49 -0600

    programas en clase

---
+++ Multiplicacion2numeros.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Multiplicacion2numeros {
+public static void main(String[] args) {
+    int a=10;
+    int b=20;
+int r=a*b;
+System.out.println(+r);
+}    
+}
---
+++ Suma2numeros.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Suma2numeros {
+public static void main(String[] args) {
+    int a=10;
+    int b=20;
+int r=a+b;
+System.out.println(+r);
+}    
+}
